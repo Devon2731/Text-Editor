a9e710ab76bd237be427fe3ffa7d9f228bbd3a1f
Taz Whitaker <tazwhitaker@Tazs-MacBook-Air.local>
2024-01-17 10:41:41 -0500

    updated and dedugged files

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,16 +1,16 @@
-const { StaleWhileRevalidate } = require('workbox-strategies');
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
-
+// Imports
+const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst } = require("workbox-strategies");
+const { registerRoute } = require("workbox-routing");
+const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+const { ExpirationPlugin } = require("workbox-expiration");
+const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
+const { StaleWhileRevalidate } = require("workbox-strategies");
 
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
+  cacheName: "page-cache",
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
@@ -22,25 +22,22 @@ const pageCache = new CacheFirst({
 });
 
 warmStrategyCache({
-  urls: ['/index.html', '/'],
+  urls: ["/index.html", "/"],
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
-// TODO: Implement asset caching
+// Implemented asset caching
 registerRoute(
-  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
   new StaleWhileRevalidate({
-    cacheName: 'asset-cache',
+    // Name of the cache storage
+    cacheName: "asset-cache",
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
     ],
-  }),
+  })
 );
-
-// Implement offline fallback for other routes
-registerRoute(({ url}) => url.pathname.startsWith('/'), offlineFallback());
-
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,35 +1,31 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {
-    // store the triggered events
-    window.deferredPrompt = event;
-// remove the hidden class from the button
-    butInstall.classList.toggle('hidden', false); 
+// Event handler for the `beforeinstallprompt` event
+window.addEventListener("beforeinstallprompt", (event) => {
+  // Stores the triggered events
+  window.deferredPrompt = event;
+
+  butInstall.classList.toggle("hidden", false);
 });
-  
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {
-    const promptEvent = window.deferredPrompt;
+// Implements a click event handler on the `butInstall` element
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.deferredPrompt;
 
-    if (!promptEvent) {
-    // The deferred prompt isn't available.
+  if (!promptEvent) {
     return;
   }
 
-  // Show the install prompt.
-    promptEvent.prompt();
+  promptEvent.prompt();
 
-    window.deferredPrompt = null;
+  // Resets the deferred prompt variable, it can only be used once
+  window.deferredPrompt = null;
 
-    butInstall.classList.toggle('hidden', true);
-});
-    
-
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {
-    window.deferredPrompt = null;
+  butInstall.classList.toggle("hidden", true);
 });
 
+// Event handler for the `appinstalled` event
+window.addEventListener("appinstalled", (event) => {
+  // Clears prompt
+  window.deferredPrompt = null;
+});
